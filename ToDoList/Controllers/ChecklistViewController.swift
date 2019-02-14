7a7b9dc00b32cefde82b38996eb9c32415a7da7c
Jordan Bonnot <jojobonnot@gmail.com>
2019-02-14 14:26:25 +0100

    Done point 6

---
+++ ToDoList/Controllers/ChecklistViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by lpiem on 14/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        cell.textLabel?.text = "Ceci est un test"
+        return cell
+    }
+}
+
