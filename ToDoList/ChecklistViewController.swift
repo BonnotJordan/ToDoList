90eb0766841c0260070526eeba480576df8a0e17
Jordan Bonnot <jojobonnot@gmail.com>
2019-02-14 14:13:25 +0100

    Done point 2

--- ToDoList/ChecklistViewController.swift
+++ ToDoList/ChecklistViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ChecklistViewController: UIViewController {
+class ChecklistViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
